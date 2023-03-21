e5f7133a255c7cf39936d57342df4bb229aa3117
NewRostov <kim.sergey.rostov@gmail.com>
2023-03-21 06:16:57 +0100

    task1.1

---
+++ task1.1/Program.cs
@@ -0,0 +1,46 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите не четное целое число от 3 до N: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        if (num < 3) Console.WriteLine("Введите число от 3 до N");
+        else if (num % 2 != 0)
+        {
+
+            int[] n = new int[num];
+            for (int i = 0; i < num; i++)
+            {
+                Console.Write($"Введите {i + 1}-е число из {num} : ");
+                n[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            PolindromCheck(num); // вызов функции
+
+
+
+
+
+            void PolindromCheck(int num) // функция
+            {
+                int count = 0;
+
+                for (int i = 0; i < num; i++)
+                {
+                    if (n[i] == n[num - 1 - i])
+                        count++;
+
+                }
+                Console.Write("Вы ввели числа:");
+                foreach (var rezult in n)
+                    Console.Write(" " + rezult + " ");
+                if (count == n.Length) Console.WriteLine("Комбинация чисел ЯВЛЯЕТСЯ ли палиндромом");
+                else Console.WriteLine("Комбинация чисел НЕ является ли палиндромом");
+
+            }
+
+        }
+
+
+        else Console.WriteLine("Вы ввели четное число!");
+    }
+}
--- task1/Program.cs
+++ task1/Program.cs
@@ -5,52 +5,57 @@
 // 12821 -> да
 //23432 -> да
 
-Console.WriteLine("введите число 1 из 5 : " );
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите число 2 из 5 " );
-int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите число 3 из 5 " );
-int c = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите число 4 из 5 " );
-int d = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите число 5 из 5 " );
-int e = Convert.ToInt32(Console.ReadLine());
-
-
-PolindromCheck1(a, b, c, d, e);  // простое решение
-PolindromCheck2(a, b, c, d, e);    // цикл
-
-
-
-
-// простое решение
-void PolindromCheck1(int a, int b, int c, int d, int e)
+internal class Program
 {
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("введите число 1 из 5 : ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("введите число 2 из 5 ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("введите число 3 из 5 ");
+        int c = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("введите число 4 из 5 ");
+        int d = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("введите число 5 из 5 ");
+        int e = Convert.ToInt32(Console.ReadLine());
 
-int[] polidrom = {a, b, c, d, e};     
-if (polidrom[0] == polidrom[4] && polidrom[1] == polidrom[3]  ) 
-    Console.WriteLine("простое решение - число является палиндромом");
-    else  Console.WriteLine("простое решение - число НЕ является палиндромом");
 
+        PolindromCheck1(a, b, c, d, e);  // простое решение
+        PolindromCheck2(a, b, c, d, e);    // цикл
+
+
+
+
+        // простое решение
+        void PolindromCheck1(int a, int b, int c, int d, int e)
+        {
+
+            int[] polidrom = { a, b, c, d, e };
+            if (polidrom[0] == polidrom[4] && polidrom[1] == polidrom[3])
+                Console.WriteLine("простое решение - число является палиндромом");
+            else Console.WriteLine("простое решение - число НЕ является палиндромом");
+
+        }
+
+        // цикл
+        void PolindromCheck2(int a, int b, int c, int d, int e)
+        {
+
+            int i = 0;
+            int count = 0;
+            int[] polidrom = { a, b, c, d, e };
+            do
+            {
+                if (polidrom[i] == polidrom[polidrom.Length - 1 - i])
+                    count++;
+                i++;
+            }
+            while (i < polidrom.Length);
+
+            if (count == polidrom.Length) Console.WriteLine("цикл - число является ли палиндромом");
+            else Console.WriteLine("цикл - число НЕ является ли палиндромом");
+
+        }
+    }
 }
-
-// цикл
-void PolindromCheck2(int a, int b, int c, int d, int e)
-{
-
-int i = 0;
-int count = 0;
-int[] polidrom = {a, b, c, d, e}; 
-do 
-{
-if (polidrom[i] == polidrom[polidrom.Length - 1 - i]) 
-    count++;
-    i++;
-}
-while (i < polidrom.Length);
-
-if (count == polidrom.Length)  Console.WriteLine("цикл - число является ли палиндромом");
-else  Console.WriteLine("цикл - число НЕ является ли палиндромом");
-
-}
-
